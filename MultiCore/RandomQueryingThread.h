b8b4422095184361a51f033a87c2d30b7fcdeba9
HaojieSHI98 <haojieshi98@gmail.com>
2020-08-14 13:46:27 +0800

    Update RandomQueryingThread.h

--- MultiCore/RandomQueryingThread.h
+++ MultiCore/RandomQueryingThread.h
@@ -1101,8 +1101,9 @@ public:
         }
         cout<<"ok1"<<endl;
 //        double time_val = (observer.task_list[10000].first- observer.task_list[0].first)/MICROSEC_PER_SEC;
-        cout<<observer.task_list[1].first<<endl;
+//        cout<<observer.task_list[1].first<<endl;
         cout<<observer.task_list[1].second<<endl;
+        cout<<observer.task_list[1].first<<endl;
         double time_val = 10;
         observer.query_rate = observer.query_rate/time_val;
         observer.update_rate = observer.update_rate/time_val;
