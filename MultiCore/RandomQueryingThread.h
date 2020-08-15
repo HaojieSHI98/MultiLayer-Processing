c1d3b9e9d381578458df3fd735c618e2072f72b6
HaojieSHI98 <haojieshi98@gmail.com>
2020-08-15 10:33:58 +0800

    Update RandomQueryingThread.h

--- MultiCore/RandomQueryingThread.h
+++ MultiCore/RandomQueryingThread.h
@@ -1178,6 +1178,7 @@ public:
 
             if(current_time-observer.time_list[0]>=MICROSEC_PER_SEC*NUM_OBV_T)
             {
+                
                 observer.task_list.erase(observer.task_list.begin(),observer.task_list.begin()+1);
                 observer.time_list.erase(observer.time_list.begin(),observer.time_list.begin()+1);
             }
