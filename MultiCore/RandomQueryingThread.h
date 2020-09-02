fc2d5486251e2ae2cc720d4287e8ba2e411b539f
HaojieSHI98 <haojieshi98@gmail.com>
2020-09-02 15:24:32 +0800

    Update RandomQueryingThread.h

--- MultiCore/RandomQueryingThread.h
+++ MultiCore/RandomQueryingThread.h
@@ -2966,7 +2966,7 @@ public:
         std::ofstream outfile;
 
 
-        outfile.open(input_parameters.output_data_dir + "stone_outfile_auto" + (multiTestPara.suffix), std::ios_base::app);
+        outfile.open(input_parameters.output_data_dir + "stone_outfile_auto_" + configstr, std::ios_base::app);
         outfile << endl
                 << " update thread "<<num_threads_update
                 << " query thread "<<num_threads_query
